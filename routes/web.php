d18e85be49db0cf734c73dd1d7b308d4e8b5c828
not-p <sonjitmurmu85@gmail.com>
2025-11-27 23:21:11 +0600

    profile page update

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,10 @@ Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admi
 Route::post('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
 Route::get('/verify', [AdminController::class, 'ShowVerification'])->name('custom.verification.form');
 Route::post('/verify', [AdminController::class, 'VerificationVerify'])->name('custom.verification.verify');
+
+
+Route::middleware('auth')->group(function () {
+
+    Route::get('/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+    Route::post('/profile/store', [AdminController::class, 'ProfileStore'])->name('profile.store');
+});
